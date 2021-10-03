42923f4122abc95713447943b98d270f80ee27f6
hsin-yu <tina2793778@gmail.com>
2021-10-03 16:22:03 +0800

    新增前台部落格樣式

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,20 +1,70 @@
 <template>
   <Loading :active="isLoading" :z-index="1080" :loader="'dots'" :color="'#384D48'"/>
-  <div class="container">
-    <h1>
+  <div class="article container mt-5 py-5">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <router-link to="/">首頁</router-link>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+          <router-link to="/blogs">部落格</router-link>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+          {{ article.title }}
+        </li>
+      </ol>
+    </nav>
+    <img :src="article.image" :alt="article.title" class="img-fluid w-100 mb-5">
+    <h1 class="border-bottom pb-3">
       {{ article.title }}
     </h1>
-    <img :src="article.image" :alt="article.title" class="img-fluid w-100">
-    <div class="d-flex justify-content-between">
-      <small>作者： {{ article.author }}</small>
-      <small>發布日期：<time>{{ $filters.date(article.create_at) }}</time></small>
+    <div class="d-md-flex justify-content-between mb-5 text-muted fs-5">
+      <div class="mb-3">
+        <small class="d-block">作者： {{ article.author }}</small>
+        <small class="d-block">發布日期：<time>{{ $filters.date(article.create_at) }}</time></small>
+      </div>
+      <div class="d-flex align-items-center">
+        <small class="me-3">
+          分享此文章
+        </small>
+        <ul class="d-flex gap-3 mb-0">
+          <li>
+            <a href="#" class="bi bi-share-fill text-muted" @click.prevent></a>
+          </li>
+          <li>
+            <a href="#" class="bi bi-facebook text-muted" @click.prevent></a>
+          </li>
+          <li>
+            <a href="#" class="bi bi-instagram text-muted" @click.prevent></a>
+          </li>
+          <li>
+            <a href="#" class="bi bi-twitter text-muted" @click.prevent></a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <article class="article" v-html="article.content"></article>
+    <article class="article-content" v-html="article.content"></article>
     <template v-for="tag in article.tag" :key="tag">
-      <a href="#" class="badge d-inline-block me-2">
+      <a href="#" class="badge d-inline-block me-2 mb-5" @click.prevent>
         #{{ tag }}
       </a>
     </template>
+    <div class="d-md-flex text-center justify-content-md-between p-5 border" v-if="articleIndex !== undefined">
+      <template v-if="articleIndex - 1 >= 0">
+         <a href="#" class="btn btn-link articles-link btn-zindex ps-0 text-start" @click.prevent="changeArticlePage(articleIndex - 1)">
+          <i class="bi bi-chevron-left"></i>
+          上一篇：
+          {{ articles[articleIndex - 1].title }}
+        </a>
+      </template>
+      <template v-if="articleIndex + 1 < articles.length">
+        <a href="#" class="btn btn-link articles-link btn-zindex ps-0 text-start" @click.prevent="changeArticlePage(articleIndex + 1)">
+          下一篇：
+          {{ articles[articleIndex + 1].title }}
+          <i class="bi bi-chevron-right"></i>
+        </a>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -23,7 +73,9 @@ export default {
   data () {
     return {
       article: '',
-      isLoading: false
+      isLoading: false,
+      articles: '',
+      articleIndex: 0
     }
   },
   inject: ['$httpMessageState', 'emitter'],
@@ -39,9 +91,10 @@ export default {
             return
           }
           this.article = response.data.article
+          this.getArticles()
           setTimeout(() => {
-            const image = document.querySelector('.article img')
-            const links = document.querySelectorAll('.article a')
+            const image = document.querySelector('.article-content img')
+            const links = document.querySelectorAll('.article-content a')
             if (image) {
               image.classList.add('img-fluid')
             }
@@ -57,26 +110,37 @@ export default {
           this.$httpMessageState(error, '連線錯誤')
           this.isLoading = false
         })
+    },
+    getArticles (page = 1) {
+      this.isLoading = true
+      const api = `/api/${process.env.VUE_APP_APIPATH}/articles?page=${page}`
+      this.$http.get(api)
+        .then(response => {
+          if (!response.data.success) {
+            this.$httpMessageState(response, '取得文章')
+            this.isLoading = false
+            return
+          }
+          this.articles = response.data.articles
+          this.articleIndex = this.articles.filter(article => article.id === this.article.id)[0].num - 1
+          this.isLoading = false
+        })
+        .catch(error => {
+          this.$httpMessageState(error, '連線錯誤')
+          this.isLoading = false
+        })
+    },
+    changeArticlePage (index) {
+      this.$router.push({ path: `/blog/${this.articles[index].id}` })
+      setTimeout(() => {
+        this.getArticle()
+      })
     }
   },
   mounted () {
-    console.log(this.$route.params.blogId)
     this.$http.defaults.baseURL = process.env.VUE_APP_API
     this.getArticle()
+    console.log(this.$router)
   }
 }
 </script>
-
-<style lang="scss" scoped>
-  .article img{
-    width: 100%;
-  }
-  .badge {
-    background-color: rgb(51, 51, 51);
-    color: #fff;
-    &:hover {
-      background-color: rgb(214, 214, 214);
-      color: rgb(43, 43, 43);
-    }
-  }
-</style>
--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,24 +1,55 @@
 <template>
   <Loading :active="isLoading" :z-index="1080" :loader="'dots'" :color="'#384D48'"/>
-  <div class="container">
-    <ul>
-      <li class="card mb-3" v-for="article in articles" :key="article.id">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img :src="article.image" :alt="article.title" class="img-fluid">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">{{ article.title }}</h5>
-              <p class="card-text text-truncate">{{ article.description }}</p>
-              <p class="card-text"><small class="text-muted">{{ new Date((article.create_at + 8 * 3600) * 1000)
-          .toISOString().split('T')[0] }}</small></p>
-              <router-link :to="`/blog/${article.id}`" class="btn btn-primary stretched-link btn-zindex position-absolute">查看內文</router-link>
+  <div class="banner">
+    <div class="container h-100 d-flex align-items-center justify-content-center">
+      <h1>
+        關於樂器我們可以了解 ——
+      </h1>
+    </div>
+  </div>
+  <div class="articles">
+    <div class="container py-5">
+      <ul class="row">
+        <li class="col-lg-12 mb-3 border-0 mb-5 card" v-if="articles.length">
+          <div class="row g-0">
+            <div class="col-md-7">
+              <img :src="articles[0].image" :alt="articles[0].title" class="img-fluid">
+            </div>
+            <div class="col-md-5 d-flex align-items-center">
+              <div class="card-body">
+                <h2 class="card-title mb-5">{{ articles[0].title }}</h2>
+                <p class="card-text">{{ articles[0].description }}</p>
+                <p class="card-text"><small class="text-muted">{{ new Date((articles[0].create_at + 8 * 3600) * 1000)
+            .toISOString().split('T')[0] }}</small></p>
+                <router-link :to="`/blog/${articles[0].id}`" class="btn btn-link stretched-link btn-zindex ps-0">
+                  查看內文
+                  <i class="bi bi-chevron-right"></i>
+                </router-link>
+              </div>
             </div>
           </div>
-        </div>
-      </li>
-    </ul>
+        </li>
+        <li class="col-lg-4 card mb-3 border-0" v-for="article in articles.slice(1)" :key="article.id">
+          <div class="row g-0">
+            <div class="col-md-12 ratio ratio-4x3">
+              <img :src="article.image" :alt="article.title" class="img-fluid card-img">
+            </div>
+            <div class="col-md-12">
+              <div class="card-body">
+                <h5 class="card-title">{{ article.title }}</h5>
+                <p class="card-text text-truncate">{{ article.description }}</p>
+                <p class="card-text"><small class="text-muted">{{ new Date((article.create_at + 8 * 3600) * 1000)
+            .toISOString().split('T')[0] }}</small></p>
+                <router-link :to="`/blog/${article.id}`" class="btn btn-link stretched-link btn-zindex ps-0">
+                  查看內文
+                  <i class="bi bi-chevron-right"></i>
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -32,7 +63,6 @@ export default {
   },
   inject: ['$httpMessageState', 'emitter'],
   methods: {
-    // 取得文章列表
     getArticles (page = 1) {
       this.isLoading = true
       const api = `/api/${process.env.VUE_APP_APIPATH}/articles?page=${page}`
@@ -58,9 +88,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-  .btn-zindex {
-    z-index: 10;
-  }
-</style>
